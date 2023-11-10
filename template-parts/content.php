1b6ff633dde2f05cdd2e0241305a1d99096c59e9
alastair38 <alastair@alastaircox.com>
2023-11-10 13:02:20 +0000

    Limit citations + comments display to posts

--- template-parts/content.php
+++ template-parts/content.php
@@ -43,7 +43,7 @@
 			
 			
 				<?php //show citations from academic citation plugin
-				if (function_exists('outputCitations') && is_singular()):?>
+				if (function_exists('outputCitations') && is_single()):?>
 					<div class="citations-block">
 					<?php echo outputCitations();?>
 					</div>
@@ -92,7 +92,7 @@
 	 * Output comments wrapper if it's a post, or if comments are open,
 	 * or if there's a comment number – and check for password.
 	 */
-	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
+	if ( ( is_single() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
 		?>
 
 		<div class="comments-wrapper section-inner">
